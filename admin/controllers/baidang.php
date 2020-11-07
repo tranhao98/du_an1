3d5b10fd6b086c7e4a614a7996c527252ffca662
tranhao98 <73809107+tranhao98@users.noreply.github.com>
2020-11-07 13:29:06 +0700

    Update baidang.php

--- admin/controllers/baidang.php
+++ admin/controllers/baidang.php
@@ -1,4 +1,36 @@
-<h1>Hào</h1>
-<h2>Khang</h2>
-<h1>Chung</h1>
-<h1>Phong</h1>
+<?php
+session_start();
+require_once "model/model_baidang.php"; //nạp model để có các hàm tương tác db 
+
+$act = "index"; //chức năng mặc định
+$message = "";
+if (isset($_GET["act"]) == true) $act = $_GET["act"]; //tiếp nhận chức năng user request
+switch ($act) {
+  case "index":
+    $view = "views/baidang-index.php";
+    require_once "layout.php";
+    break;
+  case "addnew":
+    $view = "views/baidang-addnew.php";
+    require_once "layout.php";
+    break;
+  case "edit":
+
+    $view = "views/baidang-edit.php";
+    require_once "layout.php";
+    break;
+  case "insert":
+    $view = "views/baidang-index.php";
+    require_once "layout.php";
+    break;
+  case "update":
+    
+    $view = "views/baidang-index.php";
+    require_once "layout.php";
+    break;
+  case "delete":
+    
+    $view = "views/baidang-index.php";
+    require_once "layout.php";
+    break;
+}
