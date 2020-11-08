19d3ce181a6785d22596c1289afd25c5af881384
chunghvps13121 <hoangchung0912@gmail.com>
2020-11-08 14:00:02 +0700

    Update danhmuc.php

---
+++ admin/controllers/danhmuc.php
@@ -0,0 +1,22 @@
+<?php
+  require_once "models/model_danhmuc.php"; //nạp model để có các hàm tương tác db 
+  $act = "index";//chức năng mặc định
+  $message = "";
+  if(isset($_GET["act"])==true) $act=$_GET["act"];//tiếp nhận chức năng user request
+  switch ($act) {
+
+    case "index":
+        $dsdm = getAllDanhMuc();
+        $view = "views/danhmuc-index.php";
+        require_once "layout.php";
+    break;
+
+    case "insert":
+    break;
+
+    case "edit":
+        break;  
+      case "update":
+        break;
+
+  }
