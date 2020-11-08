8c87ef9d8d36fe52225aed9a9cda87359a2a98d6
phongnt27 <73923757+phongnt27@users.noreply.github.com>
2020-11-08 12:34:47 +0700

    Update layout.php

--- admin/layout.php
+++ admin/layout.php
@@ -7,10 +7,14 @@
     <title>Document</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+    
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"> </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
@@ -116,8 +120,8 @@
                         </div>
                         <div id="collapse4" class="collapse" aria-labelledby="heading4" data-parent="#accordionExample">
                             <div class="card-body pl-5">
-                                <p><a class="btn btn-white" href="">Danh sách thông báo</a></p>
-                                <p><a class="btn btn-white" href="">Thêm thông báo</a></p>
+                                <p><a class="btn btn-white" href="?ctrl=thongbao&act=index">Danh sách thông báo</a></p>
+                                <p><a class="btn btn-white" href="?ctrl=thongbao&act=addnew">Thêm thông báo</a></p>
                             </div>
                         </div>
                     </div>
@@ -138,7 +142,9 @@
                         </nav>
                     </div>
                 </div>
-                <?php if (isset($view) && file_exists($view)) require_once "$view"; ?>
+                <div class="row">
+                    <?php if (isset($view) && file_exists($view)) require_once "$view"; ?>
+                </div>
             </main>
         </div>
         <footer></footer>
