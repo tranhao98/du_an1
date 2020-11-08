23db78c829b242419988c5408ad31500a4984094
phongnt27 <73923757+phongnt27@users.noreply.github.com>
2020-11-08 18:50:01 +0700

    Update khuvuc.php

---
+++ admin/controllers/khuvuc.php
@@ -0,0 +1,22 @@
+<?php
+  require_once "models/model_khuvuc.php"; //nạp model để có các hàm tương tác db 
+  $act = "index";//chức năng mặc định
+  $message = "";
+  if(isset($_GET["act"])==true) $act=$_GET["act"];//tiếp nhận chức năng user request
+  switch ($act) {
+
+    case "index":
+        $dskv = getAllKhuVuc();
+        $view = "views/khuvuc-index.php";
+        require_once "layout.php";
+    break;
+
+    case "insert":
+    break;
+
+    case "edit":
+        break;  
+      case "update":
+        break;
+
+  }
