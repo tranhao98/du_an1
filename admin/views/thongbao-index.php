5be59c2c6b59856e08f6237531dcb462279ff6e9
tranhao98 <73809107+tranhao98@users.noreply.github.com>
2020-11-09 13:20:30 +0700

    Update thongbao-index.php

--- admin/views/thongbao-index.php
+++ admin/views/thongbao-index.php
@@ -29,7 +29,7 @@
                     <td>
                         <?= ($row['anhien'] == 1) ? '<i style="font-size:20px" class="fa">&#xf046;</i>' : '<i style="font-size:20px" class="fa">&#xf096;</i>'; ?>
                     </td>
-                    <td class="align-middle"> <a href="index.php?ctrl=xe&act=edit&id=<?= $row['id'] ?>" class="btn-primary btn"><i class="fa fa-pencil" style="font-size:24px"></i></a> </td>
+                    <td class="align-middle"> <a href="index.php?ctrl=xe&act=edit&id=<?= $row['id'] ?>" class="btn-primary btn"><i class='fas fa-pencil-alt' style='font-size:24px'></i></a> </td>
                     <td class="align-middle"> <a onclick="return confirm('Bạn có muốn xóa không?')" href="index.php?ctrl=xe&act=delete&id=<?= $row['id'] ?>" class="btn-primary btn"><i class="fa fa-trash" style="font-size:24px"></i></a> </td>
                 </tr>
         </tbody>
