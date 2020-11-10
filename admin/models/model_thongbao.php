07944b5054138c27fe53f675fdc9a1c16364c6c5
phongnt27 <ntp27052000@gmail.com>
2020-11-10 18:50:19 +0700

    Update model_thongbao.php

--- admin/models/model_thongbao.php
+++ admin/models/model_thongbao.php
@@ -15,6 +15,18 @@
         $sql="SELECT * from thongbao where idtb='$idtb'";
         return queryOne($sql);
     }
+    
+    function updateThongBao($idtb, $idnguoidung, $tieude, $ngaydang, $noidung, $thutu, $anhien) {
+        try {
+          $sql="UPDATE thongbao SET idtb='$idtb', idnguoidung='$idnguoidung', tieude='$tieude', ngaydang='$ngaydang' ,noidung='$noidung' ,thutu='$thutu',
+          anhien='$anhien' WHERE idtb='$idtb'";
+          execute($sql);
+        }
+        catch (Exception  $e) {                         
+          print_r($e->errorInfo);
+          exit();
+        }
+    }
     function deleteThongBao($idtb){
         $sql= "delete from thongbao where idtb='$idtb'";
         execute($sql);
