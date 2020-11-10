c3b64dd03f0c2163c456989e7b4d1e7ac3e63a57
Anhenxa <73923628+maninlove0014@users.noreply.github.com>
2020-11-10 21:59:07 +0700

    Update nguoidung-edit.php

--- admin/views/nguoidung-edit.php
+++ admin/views/nguoidung-edit.php
@@ -1,11 +1,24 @@
 <div class="container">
-    <div class="row">
-        <div class="col-4 mt-4">
-        <h3>Sửa thông tin người dùng </h3>
+    <div class="row content shadow-sm mb-3 bg-white rounded">
+        <div class="col-6 text-left h-100 p-3 mt-n1">
+            <h5 class="font-weight-bold">Danh sách người dùng</h5>
+        </div>
+        <div class="col-6 h-100">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb pl-5 bg-white">
+                    <li class="breadcrumb-item active" aria-current="page">Danh sách người dùng</li>
+                    <li class="breadcrumb-item active" aria-current="page">Thay đổi thông tin người dùng</li>
+                    <li class="breadcrumb-item active" aria-current="page">Thêm người dùng</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <div class="row shadow-sm bg-white rounded p-3">
+        <h5 class="font-weight-bold mb-3">Thay đổi thông tin người dùng</h5>
             <form enctype="multipart/form-data"  method="post" action="?ctrl=nguoidung&act=edit">
                 <div class="form-group">                       
                     <label for="">ID Người Dùng </label>
-                    <input type="text" name="name" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                    <input type="text" name="name" id="id" class="form-control" placeholder="" aria-describedby="helpId">
                 </div>
                 <div class="form-group">
                     <label for="">Tên Người Dùng</label>
@@ -43,58 +56,21 @@
                     <label for="">Vai Trò</label>
                     <input type="text" name="mota" id="" class="form-control" placeholder="" aria-describedby="helpId">
                 </div>
-                <div class="form-check">
-                    <label class="form-check-label">
-                    <input type="checkbox" class="form-check-input" name="anhien" id="" value="1" checked>
-                    Ẩn Hiện
-                    </label>
+                <div class="row">
+                <div class="form-group col-6 ">
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="anhien" id="anhien1" value="1">
+                        <label class="form-check-label" for="anhien0">Hiện</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio"  name="anhien" id="anhien0" value="0">
+                        <label class="form-check-label" for="anhien0">Ẩn</label>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <input type="submit" name="themkv" id="" class="form-control" value="Submit" aria-describedby="helpId">
                 </div>
+                <input name="idtb" value="<?=$row['idtb']?>" type="hidden">
+                <button type="reset" class="btn btn-secondary mr-2">Làm lại</button>
+                <button type="submit" class="btn btn-danger">Thêm</button>
             </form>
-        </div>
-        <div class="col-8 mt-4"> 
-            <h3>Danh sách người dùng </h3>
-            <table border="1">  
-                <thead>
-                    <tr>
-                        <th scope="col">ID người dùng</th>
-                        <th scope="col">Tên người dùng</th>
-                        <th scope="col">Ngày sinh</th>
-                        <th scope="col">Tên đăng nhập</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Mật khẩu</th>
-                        <th scope="col">Số điện thoại</th>
-                        <th scope="col">Địa chỉ</th>
-                        <th scope="col">Giới tính</th>
-                        <th scope="col">Vai trò</th>
-                        <th scope="col">Ẩn Hiện</th>
-                        <th scope="col">Edit & Delete</th>
-                    </tr>
-                </thead>                        
-                <tbody>
-                    <?php foreach ($dskv as $row) { ?>
-                    <tr>
-                        <td><?=$row['id']?></td>
-                        <td><?=$row['tennguoidung']?></td>
-                        <td><?=$row['ngaysinh']?></td>
-                        <td><?=$row['tendangnhap']?></td>
-                        <td><?=$row['email']?></td>
-                        <td><?=$row['matkhau']?></td>
-                        <td><?=$row['diachi']?></td>
-                        <td><?=$row['gioitinh']?></td>
-                        <td><?=$row['vaitro']?></td>
-                        <td><?=$row['thutu']?></td>
-                        <td> <?=($row['anhien']==1)? 'Ẩn':'Hiện'; ?></td>
-                        <td>
-                            <a class="iconedit" href="?ctrl=nguoidung&act=edit&idtb=<?= $row['idkv']?>"><i style='font-size:15px' class='far'>&#xf044;</i></a>
-                            <a class="icondelete" href="?ctrl=nguoidung&act=delete&idtb=<?= $row['idkv']?>"><i style='font-size:15px' class='far'>&#xf2ed;</i></a>
-                        </td>
-                    </tr>
-                    <?php }?>
-                </tbody>
-            </table>
-        </div>
     </div>
 </div>
