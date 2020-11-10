3cb21b020bc71686667cbc5c2c1826270a845fc4
phongnt27 <ntp27052000@gmail.com>
2020-11-10 18:50:16 +0700

    Update thongbao.php

--- admin/controllers/thongbao.php
+++ admin/controllers/thongbao.php
@@ -1,4 +1,5 @@
 <?php
+  require_once "models/model_nguoidung.php";
   require_once "models/model_thongbao.php"; //nạp model để có các hàm tương tác db 
   $act = "index";//chức năng mặc định
   $message = "";
@@ -6,42 +7,45 @@
   switch ($act) {
 
     case "index":
-        $dstb = getAllThongBao();
-        $view = "views/thongbao-index.php";
-        require_once "layout.php";
-    break;
+      $dstb = getAllThongBao();
+      $nguoidung = getAllNguoiDung();
+      $view = "views/thongbao-index.php";
+      require_once "layout.php";
+      break;
 
     case "addnew":
-        $view = "views/thongbao-addnew.php";
-        require_once "layout.php";
-    break;
+      $view = "views/thongbao-addnew.php";
+      require_once "layout.php";
+      break;
 
     case "insert":
-    break;
+      break;
 
     case "edit":
-          //$thongbao =getAllThongBao();
-          //$idtb = $_GET["idtb"];
-          //settype($idtb, "int");
-          //$row = getThongBaoByID($idtb);
-          //$view = "view/ThongBao-edit.php";
-          require_once "layout.php";
-        break;  
-      case "update":
-        //   $idtb = $_POST["idtb"];
-        //   $idnguoidung = $_POST["idnguoidung"];
-        //   $tieude = $_POST["tieude"];
-        //   $ngaydang = $_POST["ngaydang"];
-        //   $noidung = $_POST["noidung"];
-        //   $thutu = $_POST["thutu"];
-        //   $anhien = $_POST["anhien"];
-        //   settype($idtb, "int");
-        //   settype($idnguoidung, "int");
-        //   settype($thutu, "int");
-        //   $noidung = trim(strip_tags($noidung));
-        //   updateThongBao($idtb, $idnguoidung, $tieude, $ngaydang, $noidung, $thutu, $anhien);
-        //   $message = "Cap nhat thanh cong!";
-          header("location:index.php?ctrl=thongbao");
-        break;
+      $thongbao = getAllThongBao();
+      // $row = getThongBaoByID($idtb);
+      // $nguoidung = getAllNguoiDung($id);
+      // $all_nd = getAllNguoiDung();
+      $idtb = $_GET["idtb"];
+      settype($idtb, "int");
+      $view = "views/thongbao-edit.php";
+      require_once "layout.php";
+      break;  
+    case "update":
+      $idtb = $_POST["idtb"];
+      $idnguoidung = $_POST["idnguoidung"];
+      $tieude = $_POST["tieude"];
+      $ngaydang = $_POST["ngaydang"];
+      $noidung = $_POST["noidung"];
+      $thutu = $_POST["thutu"];
+      $anhien = $_POST["anhien"];
+      settype($idtb, "int");
+      settype($idnguoidung, "int");
+      settype($thutu, "int");
+      $noidung = trim(strip_tags($noidung));
+      updateThongBao($idtb, $idnguoidung, $tieude, $ngaydang, $noidung, $thutu, $anhien);
+      $message = "Cap nhat thanh cong!";
+      header("location:index.php?ctrl=thongbao");
+      break;
 
   }
