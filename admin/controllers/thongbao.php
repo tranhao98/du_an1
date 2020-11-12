f66917da236709131c25d98443e16cd8913b73b3
tranhao98 <73809107+tranhao98@users.noreply.github.com>
2020-11-12 18:53:12 +0700

    update thongbao

--- admin/controllers/thongbao.php
+++ admin/controllers/thongbao.php
@@ -1,73 +1,82 @@
 <?php
-  require_once "models/model_nguoidung.php";
-  require_once "models/model_thongbao.php"; //nạp model để có các hàm tương tác db 
-  $act = "index";//chức năng mặc định
-  $message = "";
-  $nguoidung = getAllNguoiDung();
-  if(isset($_GET["act"])==true) $act=$_GET["act"];//tiếp nhận chức năng user request
-  switch ($act) {
+require_once "models/model_nguoidung.php";
+require_once "models/model_thongbao.php"; //nạp model để có các hàm tương tác db 
+$act = "index"; //chức năng mặc định
+$message = "";
+$nguoidung = getAllNguoiDung();
+if (isset($_GET["act"]) == true) $act = $_GET["act"]; //tiếp nhận chức năng user request
+switch ($act) {
 
-    case "index":
-      $nguoidung = getAllNguoiDung();
-      $dstb = getAllThongBao();
-      $view = "views/thongbao-index.php";
-      require_once "layout.php";
-      break;
+  case "index":
+    $nguoidung = getAllNguoiDung();
+    $dstb = getAllThongBao();
+    $view = "views/thongbao-index.php";
+    require_once "layout.php";
+    break;
 
-    case "addnew":
-      $view = "views/thongbao-addnew.php";
-      require_once "layout.php";
-      break;
+  case "addnew":
+    $view = "views/thongbao-addnew.php";
+    require_once "layout.php";
+    break;
 
-    case "insert":
-      $idnguoidang = $_POST['idnguoidang'];
-      $tieude = $_POST["tieude"];
-      $ngaydang = $_POST["ngaydang"];
-      $noidung = $_POST["noidung"];
-      $anhien = $_POST["anhien"];
-      settype($idnguoidang, "int");
-      settype($thutu, "int");
-      $noidung = trim(strip_tags($noidung));
-      addNewThongBao($idnguoidang, $tieude, $ngaydang, $noidung, $thutu, $anhien);
-      header("location:index.php?ctrl=thongbao");
-      break;
+  case "insert":
+    $idnguoidang = $_POST['idnguoidang'];
 
-    case "edit":
-      $idtb = $_GET["idtb"];
-      settype($idtb, "int");
+    $hinh = $_FILES['hinh']['name'];
+    $target_file = "../upload/" . basename($hinh);
+    move_uploaded_file($_FILES['hinh']['tmp_name'], $target_file);
 
-      $thongbao = getAllThongBao();
-      $row = getThongBaoByID($idtb);
-      $nguoidung = getAllNguoiDung($row['id']);
-      $nd = getAllNguoiDung();
+    $tieude = $_POST["tieude"];
+    $ngaydang = $_POST["ngaydang"];
+    $noidung = $_POST["noidung"];
+    $anhien = $_POST["anhien"];
+    settype($idnguoidang, "int");
+    settype($thutu, "int");
+    $noidung = trim(strip_tags($noidung));
+    addNewThongBao($idnguoidang, $hinh, $tieude, $ngaydang, $noidung, $thutu, $anhien);
+    header("location:index.php?ctrl=thongbao");
+    break;
 
-      $view = "views/thongbao-edit.php";
-      require_once "layout.php";
-      break;  
-    case "update":
-      $idtb = $_POST["idtb"];
-      $idnguoidang = $_POST["idnguoidang"];
-      $tieude = $_POST["tieude"];
-      $ngaydang = $_POST["ngaydang"];
-      $noidung = $_POST["noidung"];
-      $thutu = $_POST["thutu"];
-      $anhien = $_POST["anhien"];
-      settype($idtb, "int");
-      settype($idnguoidang, "int");
-      settype($thutu, "int");
-      $idtb = trim(strip_tags($idtb));
-      $noidung = trim(strip_tags($noidung));
-      updateThongBao($idtb, $idnguoidang, $tieude, $ngaydang, $noidung, $thutu, $anhien);
+  case "edit":
+    $idtb = $_GET["idtb"];
+    settype($idtb, "int");
+    
+    $thongbao = getAllThongBao();
+    $row = getThongBaoByID($idtb);
+    $nguoidung = getAllNguoiDung($row['id']);
+    $nd = getAllNguoiDung();
 
-      $message = "Cập nhật thành công!";
-      header("location:index.php?ctrl=thongbao");
-      break;
-    case "delete":
-      $idtb = $_GET["idtb"];
-      settype($idtb, "int");
-      deleteThongBao($idtb);
-      $message='Xóa Thành Công!';
-      header("location:index.php?ctrl=thongbao");
-      break;
+    $view = "views/thongbao-edit.php";
+    require_once "layout.php";
+    break;
+  case "update":
+    $idtb = $_POST["idtb"];
 
-  }
+    $hinh = $_FILES['hinh']['name'];
+    $target_file = "../upload/" . basename($hinh);
+    move_uploaded_file($_FILES['hinh']['tmp_name'], $target_file);
+
+    $idnguoidang = $_POST["idnguoidang"];
+    $tieude = $_POST["tieude"];
+    $ngaydang = $_POST["ngaydang"];
+    $noidung = $_POST["noidung"];
+    $thutu = $_POST["thutu"];
+    $anhien = $_POST["anhien"];
+    settype($idtb, "int");
+    settype($idnguoidang, "int");
+    settype($thutu, "int");
+    $idtb = trim(strip_tags($idtb));
+    $noidung = trim(strip_tags($noidung));
+    updateThongBao($idtb, $idnguoidang, $hinh, $tieude, $ngaydang, $noidung, $thutu, $anhien);
+
+    $message = "Cập nhật thành công!";
+    header("location:index.php?ctrl=thongbao");
+    break;
+  case "delete":
+    $idtb = $_GET["idtb"];
+    settype($idtb, "int");
+    deleteThongBao($idtb);
+    $message = 'Xóa Thành Công!';
+    header("location:index.php?ctrl=thongbao");
+    break;
+}
--- admin/models/model_thongbao.php
+++ admin/models/model_thongbao.php
@@ -1,38 +1,41 @@
 
 
 <?php
-    require_once "../system/database.php";
-    function getAllThongBao(){
-        $sql="SELECT * FROM thongbao";
-        return query($sql);
-    }
-    function addNewThongBao($idnguoidang, $tieude, $ngaydang, $noidung, $thutu, $anhien) {
-      $sql="INSERT INTO thongbao (idnguoidang, tieude, ngaydang, noidung, thutu, anhien) 
-      VALUES ('$idnguoidang', '$tieude', '$ngaydang', '$noidung', '$thutu', '$anhien')";
-      execute($sql);
-    }
-    function getThongBaoByID($idtb) {
-        $sql="SELECT * from thongbao where idtb='$idtb'";
-        return queryOne($sql);
-    }
-    function updateThongBao($idtb, $idnguoidang, $tieude, $ngaydang, $noidung, $thutu, $anhien) {
-        try {
-          $sql="UPDATE thongbao SET idtb='$idtb', idnguoidang='$idnguoidang', tieude='$tieude', ngaydang='$ngaydang' ,noidung='$noidung' ,thutu='$thutu',
+require_once "../system/database.php";
+function getAllThongBao()
+{
+  $sql = "SELECT * FROM thongbao";
+  return query($sql);
+}
+function addNewThongBao($idnguoidang, $hinh,  $tieude, $ngaydang, $noidung, $thutu, $anhien)
+{
+  $sql = "INSERT INTO thongbao (idnguoidang, hinh, tieude, ngaydang, noidung, thutu, anhien) 
+      VALUES ('$idnguoidang','$hinh', '$tieude', '$ngaydang', '$noidung', '$thutu', '$anhien')";
+  execute($sql);
+}
+function getThongBaoByID($idtb)
+{
+  $sql = "SELECT * from thongbao where idtb='$idtb'";
+  return queryOne($sql);
+}
+function updateThongBao($idtb, $idnguoidang, $hinh, $tieude, $ngaydang, $noidung, $thutu, $anhien)
+{
+  if ($hinh != "")
+    $sql = "UPDATE thongbao SET idtb='$idtb', idnguoidang='$idnguoidang', hinh = '$hinh', tieude='$tieude', ngaydang='$ngaydang' ,noidung='$noidung' ,thutu='$thutu',
           anhien='$anhien' WHERE idtb='$idtb'";
-          execute($sql);
-        }
-        catch (Exception  $e) {                         
-          print_r($e->errorInfo);
-          exit();
-        }
-    }
-    function deleteThongBao($idtb){
-        $sql= "delete from thongbao where idtb='$idtb'";
-        execute($sql);
-    }
-    // function getNguoiDungByID($id) {
-    //     $sql="SELECT * from taikhoan where id='$id'";
-    //     return queryOne($sql);
-    // }
+  else
+    $sql = "UPDATE thongbao SET idtb='$idtb', idnguoidang='$idnguoidang', tieude='$tieude', ngaydang='$ngaydang' ,noidung='$noidung' ,thutu='$thutu',
+          anhien='$anhien' WHERE idtb='$idtb'";
+  execute($sql);
+}
+function deleteThongBao($idtb)
+{
+  $sql = "delete from thongbao where idtb='$idtb'";
+  execute($sql);
+}
+// function getNguoiDungByID($id) {
+//     $sql="SELECT * from taikhoan where id='$id'";
+//     return queryOne($sql);
+// }
 
 ?>
