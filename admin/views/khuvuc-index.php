f34b65001b10daad71940de120d428a34406a57b
tranhao98 <73809107+tranhao98@users.noreply.github.com>
2020-11-12 19:38:23 +0700

    update dm-kv

--- admin/views/khuvuc-index.php
+++ admin/views/khuvuc-index.php
@@ -60,15 +60,16 @@
                     <?php
                     $dem = 1;
                     ?>
-                     <?php foreach ($dskv as $row) { ?>
-                    <tr>
-                        <td scope="col"><?= $dem++ ?></td>
-                        <td><?= $row['tenkhuvuc'] ?></td>
-                        <td><?= $row['thutu'] ?></td>
-                        <td> <?= ($row['anhien'] == 1) ? 'Ẩn' : 'Hiện'; ?></td>
-                        <td class="align-middle"> <a href="index.php?ctrl=khuvuc&act=edit&idkhuvuc="><i class='far fa-edit' style='font-size:18px'></i></a> </td>
-                        <td class="align-middle"> <a onclick="return confirm('Bạn có muốn xóa không?')" href="index.php?ctrl=khuvuc&act=delete&idkhuvuc=<?= $row['idkhuvuc'] ?>"><i class="fa fa-trash text-danger" style="font-size:18px"></i></a> </td>
-                    </tr>
+                    <?php foreach ($dskv as $row) { ?>
+                        <tr>
+                            <td scope="col"><?= $dem++ ?></td>
+                            <td><?= $row['tenkhuvuc'] ?></td>
+                            <td><?= $row['thutu'] ?></td>
+                            <td> <em class="text-success font-weight-normal"> <?php if ($row['anhien'] == 1) echo "Đang hiện"; ?></em>
+                                <em class="text-danger font-weight-normal"> <?php if ($row['anhien'] == 0) echo "Đang ẩn"; ?></em></td>
+                            <td class="align-middle"> <a href="index.php?ctrl=khuvuc&act=edit&idkhuvuc="><i class='far fa-edit' style='font-size:18px'></i></a> </td>
+                            <td class="align-middle"> <a onclick="return confirm('Bạn có muốn xóa không?')" href="index.php?ctrl=khuvuc&act=delete&idkhuvuc=<?= $row['idkhuvuc'] ?>"><i class="fa fa-trash text-danger" style="font-size:18px"></i></a> </td>
+                        </tr>
                     <?php } ?>
                 </tbody>
             </table>
