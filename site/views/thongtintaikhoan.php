2162c30d599763ed7703968ef96e01a5e44fc89e
Anhenxa <73923628+maninlove0014@users.noreply.github.com>
2020-11-18 02:37:53 +0700

    Update thongtintaikhoan.php

---
+++ site/views/thongtintaikhoan.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/thongbao.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/owl.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+
+    <header class="">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">
+                    <img src="../../upload/logo.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">Trang chủ
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Đất nền</a>
+
+                            <div class="dropdown-menu">
+
+                                <a class="dropdown-item" href=""></a>
+
+                            </div>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Căn hộ</a>
+
+                            <div class="dropdown-menu">
+
+                                <a class="dropdown-item" href=""></a>
+
+                            </div>
+                        </li>
+
+                        <li class="nav-item"><a class="nav-link" href="">Giới thiệu</a></li>
+
+                        <li class="nav-item"><a class="nav-link" href="">Liên hệ</a></li>
+
+                        <li class="nav-item dn"><a class="nav-link" href="">Đăng nhập</a>
+
+                        </li>
+
+                        <span class="text-white pt-2">
+                            <h4 class="text-center">|</h4>
+                        </span>
+                        <li class="nav-item dk"><a class="nav-link" href="">Đăng ký</a>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    
+    <div class="banner header-text">
+        <div class="owl-banner owl-carousel">
+            <div class="banner-item-01">
+                <div class="text-content">
+                    <h4>THÔNG TIN NGƯỜI DÙNG</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="row">
+                    <div class="col-lg-6 offset-lg-3">
+                        <div class="section-heading">
+                            <h2><em>NGUYỄN THANH PHONG</h2>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 offset-lg-3">
+                        <div class="section-heading">
+                            <h2><em>THÔNG TIN CÁ NHÂN</h2>
+                        </div> 
+                    </div>
+                </div>
+
+            <div class="card-thongbao">
+                <div class="thongbao-phai mt-200">      
+                <div class="card-thongbao">
+                        <div class="tintucmoi">
+                            <h4>Nguyễn Thanh Phong</h4>
+                            <h4>Nam</h4>
+                            <h4>24-6-1996</h4>
+                            <h4>Đồng tháp</h4>
+                            <h4>74B-Tân thới hiệp-Quận 12- TP HCM</h4>
+                            <h4>0987128420</h4>
+                            <h4>phongthanh2914@gmail.com</h4>
+                            <h4>Quản trị viên</h4>
+                            <h4>11/11/2020</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="thongbao-trai mt-100">
+                    <div class="card-thongbao">
+                        <div class="tintucmoi">
+                            <h4>Tên người dùng</h4>
+                            <h4>Giới tính</h4>
+                            <h4>Ngày sinh</h4>
+                            <h4>Quê quán</h4>
+                            <h4>Địa chỉ</h4>
+                            <h4>Số điện thoại</h4>
+                            <h4>Email</h4>
+                            <h4>Vai trò</h4>
+                            <h4>Thời gian tham gia</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="../../js/jquery/jquery.min.js"></script>
+    <script src="../../js/custom.js"> </script>
+    <script src="../../js/owl.js"> </script>
+    <script src="../../js/accordions.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
