aaa3bcccd3f9ac651e3a98e2a717580f3b14fdbb
phongnt27 <ntp27052000@gmail.com>
2020-11-22 23:06:01 +0700

    Update xemthemtintuc.php

--- site/views/xemthemtintuc.php
+++ site/views/xemthemtintuc.php
@@ -7,7 +7,7 @@
                         $dem = 1;
                         foreach ($dstball as $row) {
                     ?>
-                        <div class="tabs" id='tabs-<?= $dem++ ?>'>
+                        <article class="tabs" id='tabs-<?= $dem++ ?>'>
                             <h4><?=$row['tieude']?></h4>
 
                             <p><i class="fa fa-user"></i> <?php echo getNameNguoiDung($row['idnguoidang'])['hoten'];?> &nbsp;|&nbsp; <i class="fa fa-calendar"></i> <?=$row['ngaydang']?> &nbsp;|&nbsp; <i class="fa fa-comments"></i> <?=demsoBinhLuanTheoTB($row['idtb'])?> bình luận</p>
@@ -15,7 +15,7 @@
                             <div class="nd-tintucleft">
                                 <?php
                                     if($row['hinh'] == NULL)  echo "<img src='../../upload/1.png'>";
-                                    else  echo "<img style='height: 180px; object-fit:cover; width:100%;' src='../upload/".$row['hinh']."' ><br><br>";
+                                    else  echo "<img style='height: 180px; object-fit:cover; width:100%;' src='../upload/".$row['hinh']."' >";
                                 ?>
                             </div>
 
@@ -25,20 +25,20 @@
                                     <a href="?act=chitietthongbao&idtb=<?=$row['idtb']?>">Đọc tiếp</a>
                                 </div>
                             </div>
-                        </div> 
+                        </article>
                     <?php } ?>
                     <div class="main-button" style="text-align:center;">
-                        <a href="">Nhiều hơn</a>
+                        <a class=" mt-20" href="">Nhiều hơn</a>
                     </div><br>
                 </div>
                 <div class="thongbao-phai mt-200">
                     <div class="card-thongbao">
                         <div class="tintuccu mt-20">
                             <li class="list-group-item bg-dark"><h6 class="ttmoi text-white">TIN TỨC KHÁC</h6></li>
-                            <li class="justify t-bold text-blue"><a href="">Garden Riverside: Tận hưởng chất lượng sống sinh thái bên sông</a></li>
-                            <li class="justify t-bold text-blue"><a href="">Celesta Rise đưa phong cách nghỉ dưỡng vào không gian sống</a></li>
-                            <li class="justify t-bold text-blue"><a href="">Long An chi 13.000 tỷ đồng xây dựng 8 công trình giao thông quan trọng</a></li>
-                            <li class="justify t-bold text-blue"><a href="">The Residence Phú Quốc mở bán biệt thự 2 mặt tiền tại Bãi Trường</a></li>
+                            <li class="justify text-blue"><a href="">Garden Riverside: Tận hưởng chất lượng sống sinh thái bên sông</a></li>
+                            <li class="justify text-blue"><a href="">Celesta Rise đưa phong cách nghỉ dưỡng vào không gian sống</a></li>
+                            <li class="justify text-blue"><a href="">Long An chi 13.000 tỷ đồng xây dựng 8 công trình giao thông quan trọng</a></li>
+                            <li class="justify text-blue"><a href="">The Residence Phú Quốc mở bán biệt thự 2 mặt tiền tại Bãi Trường</a></li>
                         </div>
                         <div class="tintuckhac mt-20">
                             <li class="list-group-item bg-dark"><h6 class="ttmoi text-white">TIN TỨC KHÁC</h6></li>
