c3c6a62acb69abe2e5abb053806f10726b342a70
phongnt27 <ntp27052000@gmail.com>
2020-11-30 18:10:25 +0700

    Update baidang.php

--- site/views/baidang.php
+++ site/views/baidang.php
@@ -60,7 +60,7 @@
             </div>
             <div class="card col-md-3 rounded-0 border-0">
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item bg-dark text-white font-weight-bold">Thông tin</li>
+                    <li class="list-group-item bg-dark text-white font-weight-bold">THÔNG TIN</li>
                     <li class="list-group-item"><span class="text-secondary font-weight-bold">Danh mục:</span> <?= getTenDanhMuc($rowbaidang['iddm']) ?></li>
                     <li class="list-group-item"><span class="text-secondary font-weight-bold">Khu vực:</span> <?= getTenKhuVuc($rowbaidang['idkhuvuc']); ?></li>
                     <li class="list-group-item"><span class="text-secondary font-weight-bold">Nội thất:</span> <?= $rowbaidang['noithat'] ?></li>
@@ -75,7 +75,7 @@
                     <br>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item bg-dark text-white font-weight-bold">Liên hệ người đăng</li>
+                    <li class="list-group-item bg-dark text-white font-weight-bold">LIÊN HỆ NGƯỜI ĐĂNG</li>
                     <li class="list-group-item"><span class="text-secondary font-weight-bold">Số điện thoại:</span> +84<?= getNameNguoiDung($rowbaidang['idnguoiban'])['sodienthoai'] ?></li>
                     <li class="list-group-item"><span class="text-secondary font-weight-bold">Email:</span> <?= getNameNguoiDung($rowbaidang['idnguoiban'])['email'] ?></li>
                 </ul>
@@ -83,15 +83,16 @@
         </div>
         <div class="row">
             <div class="card col-md-6 rounded-0 border-0 thongtinmota">
-                <h2>Thông tin mô tả</h2>
+                <h2>THÔNG TIN MÔ TẢ</h2>
                 <p class="card-text"><?= $rowbaidang['mota'] ?></p>
+                <p>Phòng ngủ:</span> <?= $rowbaidang['phongngu'] ?></p>
                 <p>Diện tích:</span class="text-danger"> <?= $rowbaidang['dientich'] ?> m<sup>2</sup></p>
                 <p>Nội thất:</span class="text-danger"> <?= $rowbaidang['noithat'] ?>, rèm, máy lạnh, máy giặc</p>
                 <p>Giá thuê:</span> <?= number_format($rowbaidang['gia'], 0, ",", "."); ?><sup>VND</sup><?php if (strlen(strstr(strtolower($rowbaidang['tensp']), "cho thuê")) > 0) echo '/ Tháng';
-                                                                                                                        else echo '/M<sup>2</sup>'; ?></p>
+                                                                                                                        else echo '/m <sup>2</sup>'; ?></p>
             </div>
             <div class="card col-md-6 rounded-0 border-0">
-                <h2>Thông tin bản đồ</h2>
+                <h2>THÔNG TIN BẢN ĐỒ</h2>
                 <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.4549694901184!2d106.62692827837184!3d10.852958680974394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5b4da59e47aa97a8!2zQ8O0bmcgVmnDqm4gUGjhuqduIE3hu4FtIFF1YW5nIFRydW5n!5e0!3m2!1svi!2s!4v1606040054486!5m2!1svi!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
             </div>
         </div>
